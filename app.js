63e3f39080f11359bde58f5c21a151af475e2d54
Rex Herndon <herndonrex1@gmail.com>
2024-05-15 22:14:20 -0500

    added demo of path module

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const path = require('path');
+var pathObj = path.parse(__filename);
+console.log(pathObj);
 
-const log = require('./logger');
-
-log('message');
+// const log = require('./logger');
+// log('message');
