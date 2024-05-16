fc94f71602d06a99cb641d4377c05a77dd20a2c6
Rex Herndon <herndonrex1@gmail.com>
2024-05-15 23:16:29 -0500

    hello world :)

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 
-const EventEmitter = require('events'); // class
+const http = require('http');
 
-const Logger = require('./logger')
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
 
-// register a listener
-logger.on('messageLogged', (arg) => {
-    console.log('Listener called', arg);
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
 });
 
-logger.log('message');
+server.listen(3000);
+
+console.log('Listening on port 3000...');
 
 
 
 
 
 
+
+
+
+
+
+// =========================================
+
+// event demo
+// const EventEmitter = require('events'); // class
+// const Logger = require('./logger')
+// const logger = new Logger();
+// // register a listener
+// logger.on('messageLogged', (arg) => {
+//     console.log('Listener called', arg);
+// });
+// logger.log('message');
 
 // fs demo
 // const fs = require('fs');
