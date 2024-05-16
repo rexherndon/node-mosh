3d677440a5e3c0b715877d55c4d8635c673e2f19
Rex Herndon <herndonrex1@gmail.com>
2024-05-15 22:21:19 -0500

    os module demo

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const path = require('path');
-var pathObj = path.parse(__filename);
-console.log(pathObj);
 
+const os = require('os');
+
+var totalMemory = os.totalmem()
+var freeMemory = os.freemem()
+
+// template string
+// ES6 /ES2015 : ECMAScript 6
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
+
+
+
+// path module demo
+// const path = require('path');
+// var pathObj = path.parse(__filename);
+// console.log(pathObj);
+
+// custom module demo
 // const log = require('./logger');
 // log('message');
